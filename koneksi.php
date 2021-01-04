d105ecaa9016a09aff81b9a588521f87b45b3d0a
EC2 Default User <ec2-user@ip-172-31-36-199.ap-southeast-1.compute.internal>
2021-01-04 07:38:49 +0000

    remove rds connection

--- koneksi.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$conn = mysqli_connect("my-stage.cciibmbisxy8.ap-southeast-1.rds.amazonaws.com","admin","anel2204","digilib");
-	// $conn = mysqli_connect("localhost","root","","digilib");
-?>
