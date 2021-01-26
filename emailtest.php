7170ee32a811770c89792eba08d796d6c757134b
danielraysa <anel.raysa@gmail.com>
2021-01-26 17:04:11 +0000

    add phpmailer

---
+++ emailtest.php
@@ -0,0 +1,6 @@
+<?php
+include 'function.php';
+
+kirimEmail('daniel@dinamika.ac.id', 'Testing phpmailer', '<h2>Hello World</h2>');
+
+?>
--- function.php
+++ function.php
@@ -1,4 +1,11 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
 function check_session($url = null, $dir = null){
     global $root_folder;
     if(!isset($_SESSION['user_id']) || $_SESSION['user_id'] == ""){
@@ -10,6 +17,7 @@ function check_session($url = null, $dir = null){
         exit;
     }
 }
+
 function generateRandomString($length = 20) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
@@ -18,5 +26,36 @@ function generateRandomString($length = 20) {
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
+}
+
+function kirimEmail($tujuan, $subjek, $konten){
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'anelzraysa@gmail.com';                     // SMTP username
+        $mail->Password   = 'buxkqhvgvkxajvxf';                               // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress($tujuan); // Name is optional
+
+        // Content
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = $subjek;
+        $mail->Body    = $konten;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 ?>
